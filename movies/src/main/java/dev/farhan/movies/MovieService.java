d0948008ef8eabcb7d7995476ae4fa893df1ee99
couman05 <emanuelcoman05@yahoo.com>
2023-09-19 15:39:21 +0300

    Service class

---
+++ movies/src/main/java/dev/farhan/movies/MovieService.java
@@ -0,0 +1,29 @@
+package dev.farhan.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies()
+    {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId)
+    {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+
+}
